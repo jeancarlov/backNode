b34402d92efca739e2c8fc05f7307f8e0e467898
Jean Valderrama <jeanvalderrama@jeans-mbp-3.home>
2021-04-02 13:57:31 -0400

    controller methods are completed now it need to be connected with the routes file by bringing in the methods in the route file

---
+++ controllers/bootcamps.js
@@ -0,0 +1,50 @@
+// create different methods associate with different routes. foreach method export
+
+
+// This are middleware 
+// @Description Get all bootcamps 
+// @ Route GET/api/v1/bootcamps
+// @access public
+
+exports.getBootcamps = ( req, res, next) => {
+    res
+    .status(200)
+    .json({ sucess: true, msg: "Get route request is working => show all" });
+}
+
+// @ Description single bootcamp 
+// @ Route GET/api/v1/bootcamps/:id
+// @access public
+
+exports.getBootcamp = ( req, res, next) => {
+    res.status(200).json({ sucess: true, msg: `Get result ${req.params.id}` });
+}
+
+// @ Description create new bootcamp 
+// @ Route post/api/v1/bootcamps
+// @access private  meaning login or token is necessary
+exports.createBootcamp = ( req, res, next) => {
+   res.status(200).json({
+     sucess: true,
+     msg: "Post route request is working => Creat someting ",
+   });
+}
+// @ Description update new bootcamp 
+// @ Route put/api/v1/bootcamps
+// @access private  meaning login or token is necessary
+exports.updateBootcamp = ( req, res, next) => {
+    res.status(200).json({
+      sucess: true,
+      msg: `Post route request is working => Display something ${req.params.id}`,
+    });
+}
+// @ Description Delete new bootcamp 
+// @ Route delete /api/v1/bootcamps
+// @access private  meaning login or token is necessary
+exports.deleteBootcamp = ( req, res, next) => {
+    res
+      .status(200)
+      .json({ sucess: true, msg: `Delete result ${req.params.id}` });
+}
+
+
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,35 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
-// route creation
-router.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: "Get route request is working => show all" });
-});
-router.get("/:id", (req, res) => {
-  res.status(200).json({ sucess: true, msg: `Get result ${req.params.id}` });
-});
-router.post("/", (req, res) => {
-  res
-    .status(200)
-    .json({
-      sucess: true,
-      msg: "Post route request is working => Creat someting ",
-    });
-});
 
-// Put is for updating
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({
-      sucess: true,
-      msg: `Post route request is working => Display something ${req.params.id}`,
-    });
-});
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ sucess: true, msg: `Delete result ${req.params.id}` });
-});
 
 module.exports = router;
